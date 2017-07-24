e3c0e17ed905d71a5d33b0dda4f871c16ba74db3
SeifMostafa <seifmostafa235@gmail.com>
2017-07-25 00:48:14 +0200

    big txts solved gui

---
+++ src/ManualSelectorMain.java
@@ -0,0 +1,11 @@
+public class ManualSelectorMain {
+	public static void ManualSelectorMainGO() {
+
+	}
+
+	public static void main(String[] args) {
+		// System.out.println("Hello,World!\n This is [Manual word selector]
+		// app");
+		Utils.init();	
+	}
+}
